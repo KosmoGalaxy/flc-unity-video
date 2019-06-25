d971075b9b8ec24aede4062df4d444dc2ecbaa65
kosmogalaxy <>
2019-06-25 16:52:36 +0200

    unity test scene

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+  public GvrVideoPlayerTexture player;
+
+  private bool _started;
+  
+
+  private void Start()
+  {
+    player.Init();
+  }
+
+
+  private void Update()
+  {
+    if (_started)
+      return;
+
+    if (!player.VideoReady)
+      return;
+
+    player.Play();
+    _started = true;
+  }
+}
